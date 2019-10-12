50a614b758b9012414a5edefe9bc7edfc08ac142
Dasha <36184441+dashanowosad@users.noreply.github.com>
2019-10-12 18:03:48 +0700

    Fix lab 4

--- OS/lab4/lab4.cpp
+++ OS/lab4/lab4.cpp
@@ -2,14 +2,19 @@
 #include <process.h>
 #include <string.h>
 #include <windows.h>
-
+/*
 void ls (char *s, char *str, char *istr){
 	istr = strtok(NULL, " -");
 	while(istr != NULL){
-		if (istr[0] == 'a') //скрытые файлы
+		printf("%s", istr);
+		if (strncmp(istr, "a", 1) == 0) //скрытые файлы
 			strcat(str, " /ah");
-
-		//добавить ифов 
+		else if (strncmp(istr, "c", 1) == 0) //sort from old to new
+			strcat(str, " /od");
+		else if (strncmp(istr, "t", 1) == 0) //сортировка по последней модификации;
+			strcat(str, " /tw");
+		else if (strncmp(istr, "l", 1) == 0) //вывод сведений об авторе
+			strcat(str, " /q");
 		else //подправить, не работает, если после команды пробелы
 			strcpy(str,"");
 
@@ -18,29 +23,38 @@ void ls (char *s, char *str, char *istr){
 	istr = strtok(NULL, " -");
 	}
 }
+*/
 
-
-int main(){
+int main(int argc, char** argv){
 	while(1){
 		char s[20] = "", str[20] = "";
 		char *istr;
 		int i;
-		fgets(s, sizeof(s), stdin);
-		istr = strtok(s," -");
+		//fgets(s, sizeof(s), stdin);
+		scanf("%s",s);
+		//istr = strtok(s," ");
 		
-		while(istr != NULL){
-			if (istr[0] == 'l' && istr[1] == 's') { //ls in Linux == dir in Windows with keys:
+		/*while(istr != NULL){
+			if (strncmp(istr, "ls", strlen(istr)) == 0) { //ls in Linux == dir in Windows with keys:
 				strcpy(str, "dir");
 				ls(s,str,istr);
 			}
-			
 	
 				
 			istr = strtok(NULL, " -");
-		}
-		//printf("%d\n",strlen(str));
-		//printf("fueghf");
+		}*/
+		//printf("%s",s);
+		if (strcmp(s,"ls") == 0)
+			strcat(str, "dir");
+		else if (strcmp(s,"clear") == 0)
+			strcpy(str, "cls");
+		else if (strcmp(s,"ps") == 0)
+			strcpy(str, "tasklist");
+		else if (strcmp(s,"ip") == 0)
+			strcpy(str, "ipconfig");
 		
+		
+	
 		
 		if (strlen(str) != 0){
    			system("cls");
@@ -50,7 +64,7 @@ int main(){
 		
 		else{
 			//MessageBox(NULL,"Wrong command!","Message", MB_OK);
-			printf("Wrong command!");
+			printf("Wrong command!\n");
 		}
 			
 
