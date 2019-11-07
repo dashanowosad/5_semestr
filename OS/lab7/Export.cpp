80b5f12c06bee68b1b44e3a5f86f3d907d32a1cc
Yo265 <32211986+Yo265@users.noreply.github.com>
2019-11-08 02:35:47 +0700

    Finally complite 7 lab!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ OS/lab7/Export.cpp
@@ -0,0 +1,48 @@
+#include <windows.h>
+#include <imagehlp.h>
+#include <stdio.h>
+
+//kernel32.dll è c:\\Windows\\system32\\notepad.exe     D:\\OS\\lab7\\lab7a\\Prog1 with DLL\\MyDll\\Release\\MyDll.dll
+
+int main(void){
+	LOADED_IMAGE LoadedImage;
+	PUCHAR BaseAddress;
+	DWORD RVAExpDir, VAExpAddress;
+	IMAGE_EXPORT_DIRECTORY* ExpTable;
+	char *sName;
+	DWORD nNames;
+	char *pName;
+	char **pNames;
+	DWORD i;
+	FILE *f;
+	f = fopen("information.txt","w");
+	if (!MapAndLoad("kernel32.dll",NULL,&LoadedImage,TRUE,TRUE)){
+		printf("Somthing`s wrong!\n");
+		exit(1);
+	}
+	BaseAddress = LoadedImage.MappedAddress;
+	printf("0x%d = Base Adress\n",BaseAddress);
+
+
+	RVAExpDir = LoadedImage.FileHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress;
+	printf("0x%lx - RVA\n",RVAExpDir);
+
+	VAExpAddress = (DWORD)ImageRvaToVa(LoadedImage.FileHeader,BaseAddress,RVAExpDir,NULL);
+	printf("0x%lx - VA\n",VAExpAddress);
+	ExpTable=(IMAGE_EXPORT_DIRECTORY*)VAExpAddress;
+
+	sName=(char*)ImageRvaToVa(LoadedImage.FileHeader,BaseAddress,ExpTable->Name,NULL);
+	printf("Name of PEF:  %s\n",sName);
+
+ 	pNames=(char**)ImageRvaToVa(LoadedImage.FileHeader,BaseAddress,ExpTable->AddressOfNames,NULL);
+	nNames=ExpTable->NumberOfNames;
+	printf("Exported data: \n");
+
+	for(i = 0; i < nNames; i++){
+		pName = (char*)ImageRvaToVa(LoadedImage.FileHeader,BaseAddress,(DWORD)*pNames,NULL);
+		printf("%s\n",pName);
+		*pNames++;
+	}
+	UnMapAndLoad(&LoadedImage);
+	return 0;
+}
---
+++ OS/lab7/Import.cpp
@@ -0,0 +1,57 @@
+#include <windows.h>
+#include <imagehlp.h>
+#include <stdio.h>
+
+//kernel32.dll и c:\\Windows\\system32\\notepad.exe     D:\\OS\\lab7\\lab7a\\Prog1 with DLL\\MyDll\\Release\\MyDll.dll
+
+int main(void){
+	PCSTR ImageName;
+	LOADED_IMAGE LoadedImage;
+	PUCHAR BaseAddress;
+	DWORD RVAExpDir, VAExpAddress;
+	IMAGE_IMPORT_DESCRIPTOR* ImportTable;
+	IMAGE_IMPORT_BY_NAME* Names;
+	PIMAGE_THUNK_DATA thunk;
+	PIMAGE_SECTION_HEADER pSection;
+	char *sName;
+	DWORD nNames;
+	char *pName;
+	char **pNames;
+	DWORD i;
+	LPVOID DLLName;
+	FILE *f;
+	f = fopen("information.txt","w");
+	if (!MapAndLoad("c:\\Windows\\system32\\notepad.exe ",NULL,&LoadedImage,FALSE,TRUE)){
+		printf("Somthing`s wrong!\n");
+		exit(1);
+	}
+	BaseAddress = LoadedImage.MappedAddress;
+	printf("0x%d = Base Adress\n",BaseAddress);
+
+
+	RVAExpDir = LoadedImage.FileHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress; //виртуальный адрес
+	printf("0x%lx - RVA\n",RVAExpDir);
+
+
+	VAExpAddress = (DWORD)ImageRvaToVa(LoadedImage.FileHeader,BaseAddress,RVAExpDir,NULL);//адрес виртуального массива строк
+	printf("0x%lx - VA\n",VAExpAddress);
+	ImportTable=(IMAGE_IMPORT_DESCRIPTOR*)VAExpAddress;
+
+	printf("Imported data: \n");
+
+	while(ImportTable->Name != NULL){
+		pNames=(char**)ImageRvaToVa(LoadedImage.FileHeader,BaseAddress,ImportTable->FirstThunk,NULL);
+		sName=(char*)ImageRvaToVa(LoadedImage.FileHeader,BaseAddress,ImportTable->Name,NULL); //названия библиотек dll
+		printf("Name of PEF:  %s\n",sName);
+		while(pName != 0){
+			pName = (char*)ImageRvaToVa(LoadedImage.FileHeader,BaseAddress,(DWORD)*pNames+2,NULL);
+			if(pName != 0) 
+				printf("     %s\n",pName);
+			*pNames++;
+		}
+		*pName++;
+		ImportTable++;
+	}
+	UnMapAndLoad(&LoadedImage);
+	return 0;
+}
