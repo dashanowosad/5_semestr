9fffb58b7a4c137e95e3da62c7aabae7477ca817
Тимур <nbvehrnbvehrnbvehr@gmail.com>
2019-11-08 12:07:36 +0700

    Minor changes in lab6.pl

--- FLP/lab6/lab6.pl
+++ FLP/lab6/lab6.pl
@@ -14,7 +14,7 @@
 
 
     first(List,B,C,L1,L2,L3):-
-        List = [Head|Tail],
+        List = [Head|_],
 
         (Head > C, append([Head],L2,L22); L22 = L2),
         (Head < B, append([Head],L1,L11); L11 = L1),
@@ -22,11 +22,6 @@
         select(Head,List,List2),
 
         (List2 \== [], first(List2,B,C,L11,L22,L33);write(L11), write(' '), write(L33), write(' '), write(L22)).
-
-
-
-
-
 
 
 func1:-
@@ -44,3 +39,23 @@ func1:-
   L3 = [],
   (B > C, B1 is C, C1 is B; B1 is B, C1 is C),
   first(List,B1,C1,L1,L2,L3).
+/*Задание № 2*/
+
+find_max(List,[Head|Tail],Max):-
+  Max1 is Head,
+  Max < Head,
+  find_max(List,Tail, Max1).
+
+find_max(List,[_|Tail],Max):-
+  find_max(List,Tail,Max).
+
+find_max(List,[], Max):-
+  write(Max).
+
+
+func2:-
+  List = [3,5,4,2,5,5,0],
+  List1 = List,
+  List1 = [Head|_],
+  Max is Head,
+  find_max(List,List1,Max).
