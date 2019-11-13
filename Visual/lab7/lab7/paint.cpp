5e74d9f9e41485af8122f8d13336e3323c36cb8a
Тимур <nbvehrnbvehrnbvehr@gmail.com>
2019-11-13 13:02:24 +0700

    Add shariki and snowflake

--- Visual/lab7/lab7/paint.cpp
+++ Visual/lab7/lab7/paint.cpp
@@ -3,18 +3,18 @@ Paint::Paint()
 {
 
     Polygon = new QGraphicsPolygonItem();
-    p << QPointF(40,200);
-    p << QPointF(120,200);
-    p << QPointF(90,160);
-    p << QPointF(110,160);
-    p << QPointF(90,130);
-    p << QPointF(100,130);
-    p << QPointF(80,80);
+    p << QPointF(80,400);
+    p << QPointF(240,400);
+    p << QPointF(180,320);
+    p << QPointF(220,320);
+    p << QPointF(180,260);
+    p << QPointF(200,260);
+    p << QPointF(160,160);
 
-    p << QPointF(60,130);
-    p << QPointF(70,130);
-    p << QPointF(50,160);
-    p << QPointF(70,160);
+    p << QPointF(120,260);
+    p << QPointF(140,260);
+    p << QPointF(100,320);
+    p << QPointF(140,320);
 
 
 
