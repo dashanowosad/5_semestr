8683d3d21c2efae01fee0332b104776b770bc5c5
Тимур <nbvehrnbvehrnbvehr@gmail.com>
2019-11-19 22:21:47 +0700

    Deadlock.cpp before resolving deadlock (lol)

---
+++ OS/lab10/deadlock.cpp
@@ -0,0 +1,37 @@
+#include <windows.h>
+#include <process.h>
+#include <stdio.h>
+
+HANDLE hMutex1, hMutex2;
+double sh1 = 0.0;
+int sh2 = 0;
+void Thread(void* pParams);
+
+int main(void){
+  hMutex1 = CreateMutex(NULL, FALSE, NULL);
+  hMutex2 = CreateMutex(NULL, FALSE, NULL);
+
+  _beginthread(Thread, 0, NULL);
+
+  while(1){
+    WaitForSingleObject(hMutex1, INFINITE); //capture
+      printf("%g\n", sh1);
+    WaitForSingleObject(hMutex2, INFINITE); //capture
+      printf("%d\n", sh2);
+    ReleaseMutex(hMutex2); //exemption
+    ReleaseMutex(hMutex1); //exemption
+  }
+  return 0;
+}
+
+
+void Thread(void* pParams){
+  while (1) {
+    WaitForSingleObject(hMutex2, INFINITE); //mutex capture
+      sh2++;
+    WaitForSingleObject(hMutex1, INFINITE); //mutex capture
+      sh1 += 0.1;
+    ReleaseMutex(hMutex1); //mutex release
+    ReleaseMutex(hMutex2); //mutex release
+  }
+}
