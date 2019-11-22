d3bebd1bf3fd93724c7ae1711b3e622be1a117c6
Yo265 <nbvehrnbvehrnbvehr@gmail.com>
2019-11-22 22:48:31 +0700

    Complite DEGMV

--- ABC/lab5/BLAS.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by asakujaku on 15.11.2019.
-//
-
-#include "BLAS.h"
--- ABC/lab5/BLAS.h
+++ ABC/lab5/BLAS.h
@@ -1,12 +1,10 @@
 //
-// Created by asakujaku on 15.11.2019.
+// Created by asakujaku on 22.11.2019.
 //
 
 #ifndef LAB5_BLAS_H
 #define LAB5_BLAS_H
 
-void DEGMV(int N, int M, int K, double* A, int la, double alpha, double beta, double *B, double* C, int lc);
-
-
+void DEGMV(int N, double* A, int la, double alpha, double beta, double *B, double* C);
 
 #endif //LAB5_BLAS_H
