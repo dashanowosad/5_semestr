156c97d6a50bfd2c87791e98ea825e849e4a8185
Dasha <dashanowosad@yandex.ru>
2019-12-02 10:21:15 +0700

    Complete lab6 AVS

--- ABC/lab6/main.c
+++ ABC/lab6/main.c
@@ -27,69 +27,57 @@ void Fill_matrix_line(double *restrict x, double *restrict y, int N){
 int main() {
     double **x, **y, **c;
     double *x1, *y1, *c1;
-    int scalar = 1, N = 1000, BlockSize = 16;
-    FILE *stat;
-    stat = fopen("../3.txt", "w");
-    clock_t start1, stop;
+    int scalar = 1, N = 1000, BlockSize;
+    FILE *stat, *stat12;
+   /* stat = fopen("../3.txt", "w");
+    stat12 = fopen("../1_2.txt", "w");*/
+    clock_t start1, stop1, start, stop;
     srand(time(NULL));
 
-    /*x = (double**)calloc(sizeof(double),N);
-    y = (double**)calloc(sizeof(double),N);
-    c = (double**)calloc(sizeof(double),N);
-    for(int i = 0; i < N; i++){
-        x[i] = (double*)calloc(sizeof(double),N);
-        y[i] = (double*)calloc(sizeof(double),N);
-        c[i] = (double*)calloc(sizeof(double),N);
+    /*x = (double **) calloc(sizeof(double), N);
+    y = (double **) calloc(sizeof(double), N);
+    c = (double **) calloc(sizeof(double), N);
+    for (int i = 0; i < N; i++) {
+        x[i] = (double *) calloc(sizeof(double), N);
+        y[i] = (double *) calloc(sizeof(double), N);
+        c[i] = (double *) calloc(sizeof(double), N);
     }
-    Fill_matrix(x,y,N);
-    DEGMM_double_v2(N,N,N,scalar,x,N,y,N,scalar,c,N);
-    for(int i = 0; i < N; i++){
-        free(x[i]);
-        free(y[i]);
-        free(c[i]);
-    }
-    free(x);
-    free(y);
-    free(c);*/
+    Fill_matrix(x, y, N);
 
+    //start = clock();
+   // DEGMM_double_v1(N, N, N, scalar, x, N, y, N, scalar, c, N);
+   // stop = clock();
 
-    //for(BlockSize = 1; BlockSize < 16; BlockSize += 2) {
-        x1 = (double*)calloc(sizeof(double),N*N);
-        y1 = (double*)calloc(sizeof(double),N*N);
-        c1 = (double*)calloc(sizeof(double),N*N);
-        Fill_matrix_line(x1,y1,N);
+      // start1 = clock();
+        DEGMM_double_v2(N, N, N, scalar, x, N, y, N, scalar, c, N);
+        //stop1 = clock();*/
 
-        start1 = clock();
-        DEGMM_double_v3(N, N, N, scalar, x1, N, y1, N, scalar, c1, N, BlockSize);
-        stop = clock();
+       // fprintf(stat12, "[%d ,\t %f, \t %f ],\n", j, (double) (stop - start) / CLOCKS_PER_SEC, (double) (stop1 - start1) / CLOCKS_PER_SEC);
+        //printf("N = %d\n",j);
+    /*    for (int i = 0; i < N; i++) {
+            free(x[i]);
+            free(y[i]);
+            free(c[i]);
+        }
+        free(x);
+        free(y);
+        free(c);*/
 
-        free(x1);
-        free(y1);
-        free(c1);
-        fprintf(stat,"Block Size = %d, time = %f\n",BlockSize,(double)(stop-start1)/CLOCKS_PER_SEC);
-        printf("Block Size = %d complete\n",BlockSize);
+   // fclose(stat12);
 
-    //}
-    fclose(stat);
-    //printf("%f",(double)(stop-start1)/CLOCKS_PER_SEC);
-
-    /*for(int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++)
-            printf("%.0f ", x1[i * N + j]);
-        printf("\n");
-    }
-    printf("\n");
-    for(int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++)
-            printf("%.0f ", y1[i * N + j]);
-        printf("\n");
-    }
-    printf("\n");
-    for(int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++)
-            printf("%.0f ", c1[i * N + j]);
-        printf("\n");
-    }*/
+    x1 = (double*)calloc(sizeof(double),N*N);
+    y1 = (double*)calloc(sizeof(double),N*N);
+    Fill_matrix_line(x1,y1,N);
+    c1 = (double*)calloc(sizeof(double),N*N);
+    //start1 = clock();
+    DEGMM_double_v3(N, N, N, scalar, x1, N, y1, N, scalar, c1, N, 1024);
+  //  stop = clock();
+   // fprintf(stat,"Block Size = %d, time = %f\n",BlockSize,(double)(stop-start1)/CLOCKS_PER_SEC);
+   // printf("Block Size = %d complete\n",BlockSize);
+    /*free(x1);
+    free(y1);
+    free(c1);*/
+  //  fclose(stat);
 
 
     return 0;
